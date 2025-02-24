56daa0889608a9826f32891fc0d358f5fcab0f64
NguyenGiaBao <bao.ng05653@sinhvien.hoasen.edu.vn>
2025-02-25 03:27:42 +0700

    feat:boos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {BookDetailsComponent} from './pages/book-details/book-details.component';
+    import { HomeComponent } from './pages/home/home.component';
+    import { BookDetailsComponent } from './pages/book-details/book-details.component';
 
-export const routes: Routes = [
-  {
-    path:"",
-    component: HomeComponent,
-  },
-  {
-    path: "book-details/:number",
-    component: BookDetailsComponent
-  },
-  {
-    path:"home",
-    component: HomeComponent
-  }
-
-];
+    export const routes: Routes = [
+      {
+        path: "",
+        component: HomeComponent,
+      },
+      {
+        path: "book-details/:number",
+        component: BookDetailsComponent,
+      },
+      {
+        path: "home",
+        component: HomeComponent,
+      },
+    ];
